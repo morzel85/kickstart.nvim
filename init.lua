b0cc1c7f164ef6bfa9cd66c4b3d5596b3d3e9b43
morzel85 <milosz.orzel@gmail.com>
2024-12-08 12:49:56 +0100

    pest.vim and autopairs plugins added

--- init.lua
+++ init.lua
@@ -958,6 +958,9 @@ require('lazy').setup({
     version = '^5', -- Recommended
     lazy = false, -- This plugin is already lazy
   },
+  {
+    'pest-parser/pest.vim'
+  },
 
   -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
@@ -971,7 +974,7 @@ require('lazy').setup({
   -- require 'kickstart.plugins.debug',
   -- require 'kickstart.plugins.indent_line',
   -- require 'kickstart.plugins.lint',
-  -- require 'kickstart.plugins.autopairs',
+  require 'kickstart.plugins.autopairs',
   require 'kickstart.plugins.neo-tree',
   -- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
 
