00aa3242998b74b7420ce1dc60deb4dac032c7b8
morzel85 <milosz.orzel@gmail.com>
2025-01-06 11:06:01 +0100

    mini.bracketed enabled

--- init.lua
+++ init.lua
@@ -723,6 +723,7 @@ require('lazy').setup({
       -- - sd'   - [S]urround [D]elete [']quotes
       -- - sr)'  - [S]urround [R]eplace [)] [']
       require('mini.surround').setup()
+      require('mini.bracketed').setup()
 
       -- Simple and easy statusline.
       --  You could remove this setup call if you don't like it,
