97f8808d8399a5e1a3c8f9310352fd1f33c2ee5e
morzel85 <milosz.orzel@gmail.com>
2025-03-28 23:47:15 +0100

    Code folding enabled (treesitter)

--- init.lua
+++ init.lua
@@ -839,3 +839,13 @@ require('lazy').setup({
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
+--
+
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = '*',
+  callback = function()
+    vim.wo.foldmethod = 'expr'
+    vim.wo.foldexpr = 'nvim_treesitter#foldexpr()'
+    vim.wo.foldlevel = 99
+  end,
+})
